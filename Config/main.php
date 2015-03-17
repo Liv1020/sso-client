8230563011b96755c8a177e82a2a4fb4ee210817
Liv <523260513@qq.com>
2015-03-17 10:35:06 +0800

    add files

---
+++ Config/main.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: Liv
+ * Date: 15/3/17
+ * Time: 上午10:28
+ */
+
+return array(
+    'phpsso' => '1',	//是否使用phpsso
+    'phpsso_appid' => '1',	//应用id
+    'phpsso_api_url' => 'http://livphpcms.com/phpsso_server',	//接口地址
+    'phpsso_auth_key' => 'VKar7Ct8GlteB8TgpKZP79ewqA1VIc5x', //加密密钥
+    'phpsso_version' => '1', //phpsso版本
+);
